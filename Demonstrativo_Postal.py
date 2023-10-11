6882a31966396d4b6d1e5bbccd7131b65f525b10
Lucas <lp.paulopaz@gmail.com>
2023-10-11 11:05:08 -0300

    fix

--- Demonstrativo_Postal.py
+++ Demonstrativo_Postal.py
@@ -8,7 +8,7 @@ from selenium import webdriver
 from tkinter import filedialog
 from seleniumwire import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.edge.options import Options
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import Pidgin
