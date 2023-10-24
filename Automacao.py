322a872c18e1ee074f85c8e96a689ebac8a1d670
Lucas <lp.paulopaz@gmail.com>
2023-10-24 14:05:36 -0300

    fix

--- Automacao.py
+++ Automacao.py
@@ -36,10 +36,6 @@ from Demonstrativo_Stf import demonstrativo_stf
 from Demonstrativo_Tjdft import demonstrativo_tjdft
 from Demonstrativo_Unafisco import demonstrativo_unafisco
 from Nota_Fiscal import subirNF
-<<<<<<< HEAD
-=======
-from gerador_de_planilha import gerar_planilha
->>>>>>> a2cc4153f0e359d95497fda2f6934e01efaba6dd
 from datetime import datetime
 import os
 
@@ -346,7 +342,7 @@ class Application:
 
         elif automacao == "Glosa - Gerador de Planilha GDF":
             self.gif()
-            gerar_planilha()
+            ...
             self.reiniciar
 
         elif automacao == "Glosa - Recursar GEAP Duplicado":
