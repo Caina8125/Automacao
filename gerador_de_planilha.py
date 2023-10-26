07bcb36f5c628aca2ee7e4511a1a67a7c18d2d7f
Lucas <lp.paulopaz@gmail.com>
2023-10-26 14:52:34 -0300

    fix recurso serpro

--- gerador_de_planilha.py
+++ gerador_de_planilha.py
@@ -1,38 +1,103 @@
 import pandas as pd
 from tkinter import filedialog
+from datetime import datetime
+
+def verificar_frame_vazio(df_filtrado, df, coluna, numero):
+    if df_filtrado.empty == True:
+        fatura_df = df.loc[(df[coluna] == numero)]
+        return fatura_df
+        
+    else:
+        return df_filtrado
 
 def gerar_planilha():
     planilha_fhasso = filedialog.askopenfilename()
+    planilha_unificada = filedialog.askopenfilename()
     planilha_gdf = filedialog.askopenfilename()
     df_plan_fhasso = pd.read_excel(planilha_fhasso)
     df_plan_fhasso = df_plan_fhasso.loc[df_plan_fhasso["Recupera ?"] == "Sim"]
     df_plan_fhasso = df_plan_fhasso.sort_values(by=['Fatura Inicial'])
+    df_plan_fhasso['Controle Recurso'] = ''
+    df_plan_unificada = pd.read_excel(planilha_unificada)
+    df_plan_unificada['INSERIDO'] = ''
     df_plan_gdf = pd.read_excel(planilha_gdf)
     lista = []
+    count_nao_encontradas = 0
 
     for index, linha in df_plan_fhasso.iterrows():
-        numero_senha_fhasso = str(linha['AUTORIZACAO']).replace('.0', '')
-        numero_op_fhasso = str(linha['GUIAATENDIMENTO']).replace('.0', '')
-        fatura_recurso = str(linha['PROCESSOID']).replace('.0', '')
-        realizado = linha['DATAREALIZADO']
-        procedimento = str(linha['CODIGOID']).replace('.0', '')
-        controle = str(linha['ATENDIMENTOID']).replace('.0', '')
+        numero_senha_fhasso = str(linha['Autorização']).replace('.0', '')
+        numero_op_fhasso = str(linha['Nro. Guia']).replace('.0', '')
+        numero_amhptiss = str(linha['Amhptiss']).replace('.0', '')
+        fatura_recurso = str(linha['Fatura Recurso']).replace('.0', '')
+        fatura_inicial = str(linha['Fatura Inicial']).replace('.0', '')
+        realizado = linha['Realizado']
+        procedimento = str(linha['Procedimento']).replace('.0', '')
+        valor_original = str(linha['Valor Original'])
+        valor_glosa = str(linha['Valor Glosa']).replace('-', '')
+        fatura_df_filtrado = df_plan_unificada.loc[(df_plan_unificada['PROCESSOID'] == int(fatura_recurso))]
+        fatura_df = verificar_frame_vazio(fatura_df_filtrado, df_plan_unificada, 'PROCESSOID', fatura_recurso)
+        fatura_df_gdf_filtrado = df_plan_gdf.loc[(df_plan_gdf['Lote Prestador'] == int(fatura_inicial))]
+        fatura_df_gdf = verificar_frame_vazio(fatura_df_gdf_filtrado, df_plan_gdf, 'Lote Prestador', fatura_inicial)
 
-        for i, l in df_plan_gdf.iterrows():
-            lista_linha = []
-            numero_senha_plan_gdf = str(l['Autorização Origem']).replace('.0', '')
-            data_de_atendimento_gdf = l['Data de Realização']
-            codigo_gdf = str(l['Código']).replace('.0', '')
-            autorizacao_nova = str(l['Autorização']).replace('.0', '')
+        if fatura_df.empty == True or fatura_df_gdf.empty == True:
+            count_nao_encontradas += 1
+            continue
+
+        for i, l in fatura_df.iterrows():
+            primeira_validacao = False
+            senha_plan_uni = str(l['AUTORIZACAO']).replace('.0', '')
+            numero_op_plan_uni = str(l['GUIAATENDIMENTO']).replace('.0', '')
+            hdi_registro = str(l['HDIREGISTRO']).replace('.0', '')
+            fatura_recurso_plan_uni = str(l['PROCESSOID']).replace('.0', '')
+            realizado_plan_uni = datetime.strptime(l['DATAREALIZADO']._repr_base.replace(' 00:00:00', ''), "%Y-%m-%d")
+            realizado_plan_uni = realizado_plan_uni.strftime('%d/%m/%Y')
+            procedimento_plan_uni = str(l['CODIGOID']).replace('.0', '')
+            controle_plan_uni = str(l['ATENDIMENTOID']).replace('.0', '')
+
+            validacao_senha_oper = numero_senha_fhasso == senha_plan_uni or numero_op_fhasso == numero_op_plan_uni
+            valicacao_amhptiss = numero_amhptiss == hdi_registro
+            validacao_fatura = fatura_recurso == fatura_recurso_plan_uni
+            validacao_data = realizado == realizado_plan_uni
+            validacao_procedimento = procedimento == procedimento_plan_uni
+
+            if validacao_senha_oper and validacao_fatura and valicacao_amhptiss and validacao_data and validacao_procedimento:
+                df_plan_fhasso.loc[index, 'Controle Recurso'] = controle_plan_uni
+                fatura_df.loc[i, 'INSERIDO'] = 'Sim'
+                primeira_validacao = True
+                break
+
+            else:
+                continue
+
+        if primeira_validacao == False:
+            count_nao_encontradas += 1
+            continue
+
+        for ind, lin in fatura_df_gdf.iterrows():
+            segunda_comparacao = False
+            numero_senha_plan_gdf = str(lin['Autorização Origem']).replace('.0', '')
+            data_de_atendimento_gdf = lin['Data de Realização']
+            codigo_gdf = str(lin['Código']).replace('.0', '')
+            autorizacao_nova = str(lin['Autorização']).replace('.0', '')
+            vl_apresentado = str(lin['Vl Apresentado (R$)']).replace('R$ ', '')
+            vl_de_glosa = str(lin['Vl de Glosa (R$)']).replace('R$ ', '')
 
             comparacao_senha = numero_senha_fhasso == numero_senha_plan_gdf
             comparacao_numero = numero_op_fhasso == numero_senha_plan_gdf
             comparacao_data = realizado == data_de_atendimento_gdf
             comparacao_codigo = procedimento == codigo_gdf
+            comparacao_valor_original = valor_original == vl_apresentado
+            comparacao_valor_glosa = valor_glosa == vl_de_glosa
 
-            if (comparacao_senha or comparacao_numero) and comparacao_data and comparacao_codigo:
-                lista_linha = [int(controle), int(autorizacao_nova), int(numero_senha_fhasso), int(numero_op_fhasso), int(fatura_recurso)]
+            if (comparacao_senha or comparacao_numero) and comparacao_data and comparacao_codigo and comparacao_valor_original and comparacao_valor_glosa:
+                controle = df_plan_fhasso['Controle Recurso'][index]
+                lista_linha = [controle, autorizacao_nova, numero_senha_fhasso, numero_op_fhasso, fatura_inicial, fatura_recurso]
+                segunda_comparacao = True
                 break
+        
+        if segunda_comparacao == False:
+            count_nao_encontradas += 1
+            continue
 
         lista.append(lista_linha)
 
@@ -41,3 +106,5 @@ def gerar_planilha():
     df_nova_planilha = pd.DataFrame(lista)
     df_nova_planilha.columns = cabecalho
     df_nova_planilha.to_excel(r'C:\Users\lucas.paz\Documents\Teste\GDF.xlsx', index=False)
+        
+gerar_planilha()
--- teste.py
+++ teste.py
@@ -1,5 +1,14 @@
 import pandas as pd
 
-planilha = r"C:\Users\lucas.paz\Documents\Cobranca_Unificada.csv"
-df = pd.read_csv(planilha)
-print(df)
+planilha = r"C:\Users\lucas.paz\Desktop\GDF - Sincronização\copy\Extrato de glosa - GDF.xlsx"
+df = pd.read_excel(planilha)
+autorizacao = df['Autorização']
+autorizacao_origem = df['Autorização Origem']
+fatura_df = df.loc[(df['Autorização'] == 3037832)]
+
+if fatura_df.empty == True:
+    fatura_df = df.loc[(df['Autorização'] == '3037832')]
+
+print(autorizacao)
+print('')
+print(autorizacao_origem)
