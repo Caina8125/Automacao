cd28d659701f46acf27f7950132c361f58cc8bbb
Lucas <lp.paulopaz@gmail.com>
2024-01-17 10:02:41 -0300

    automação

--- leitor_de_pdf_gama.py
+++ leitor_de_pdf_gama.py
@@ -17,21 +17,22 @@ class PDFReader():
     
     @directory.setter
     def directory(self, directory: str) -> None:
-        self._directory = directory
+        if type(directory) == str:
+            self._directory = directory
 
-    def main(self) -> list[list]:
+    def main(self) -> list:
         LISTA_DE_ARQUIVOS: list = PDFReader.set_list(self.directory)
         ARQUIVO_CARTA_REMESSA: str | bool = PDFReader.get_carta_remessa(LISTA_DE_ARQUIVOS)
         if(ARQUIVO_CARTA_REMESSA):
             LISTA_DE_ARQUIVOS.remove(ARQUIVO_CARTA_REMESSA)
         else:
             showinfo('Automação', 'Carta remessa não encontrada!')
-            return
+            return False
         DF_TABELA_REMESSA: pd.DataFrame = PDFReader.get_df_remessa(ARQUIVO_CARTA_REMESSA)
-        MATRIZ_DE_DADOS = []
+        MATRIZ_DE_DADOS: list = []
 
         for arquivo in LISTA_DE_ARQUIVOS:
-            arquivo_formatado = arquivo.replace(self.directory, '').replace('\\', '')
+            arquivo_formatado: str = arquivo.replace(self.directory, '').replace('\\', '')
 
             result: dict = PDFReader(self.directory).ler_pdfs(arquivo)
 
@@ -41,23 +42,23 @@ class PDFReader():
             numero_fatura_pagina2: str = PDFReader.get_numero_nota(result['pages_content_array'][1])
 
             if numero_fatura_pagina1 == numero_fatura_pagina2:
-                numeros_remessa = PDFReader.number_in_df(DF_TABELA_REMESSA, numero_fatura_pagina1, protocolo)
+                numeros_remessa: str | bool = PDFReader.number_in_df(DF_TABELA_REMESSA, numero_fatura_pagina1, protocolo)
 
                 if numeros_remessa == False:
-                    lista_de_dados: list[str] = [arquivo_formatado, numero_fatura_pagina1, protocolo, 'Não encontrado', 'Não encontrado', numero_fatura_pagina2, 'Fatura não econtrada na remessa']
+                    lista_de_dados: list = [arquivo_formatado, numero_fatura_pagina1, protocolo, 'Não encontrado', 'Não encontrado', numero_fatura_pagina2, 'Fatura não econtrada na remessa']
                 
                 else:
                     processo_remessa, protocolo_remessa = numeros_remessa
                     if(processo_remessa and protocolo_remessa):
-                        lista_de_dados: list[str] = [arquivo_formatado, numero_fatura_pagina1, protocolo, processo_remessa, protocolo_remessa, numero_fatura_pagina2, 'OK']
+                        lista_de_dados: list = [arquivo_formatado, numero_fatura_pagina1, protocolo, processo_remessa, protocolo_remessa, numero_fatura_pagina2, 'OK']
 
                     elif(processo_remessa and protocolo_remessa == False):
-                        lista_de_dados: list[str] = [arquivo_formatado, numero_fatura_pagina1, protocolo, processo_remessa, 'Inválido', numero_fatura_pagina2, 'Número de protocolo diferente']
+                        lista_de_dados: list = [arquivo_formatado, numero_fatura_pagina1, protocolo, processo_remessa, 'Inválido', numero_fatura_pagina2, 'Número de protocolo diferente']
 
                     elif(processo_remessa == False and protocolo_remessa):
-                        lista_de_dados: list[str] = [arquivo_formatado, numero_fatura_pagina1, protocolo, 'Inválido', protocolo_remessa, numero_fatura_pagina2, 'Número de processo diferente']
+                        lista_de_dados: list = [arquivo_formatado, numero_fatura_pagina1, protocolo, 'Inválido', protocolo_remessa, numero_fatura_pagina2, 'Número de processo diferente']
             else:
-                lista_de_dados: list[str] = [arquivo_formatado, numero_fatura_pagina1, protocolo, '-', '-', numero_fatura_pagina2, 'Os números de fatura no arquivo são diferentes']
+                lista_de_dados: list = [arquivo_formatado, numero_fatura_pagina1, protocolo, '-', '-', numero_fatura_pagina2, 'Os números de fatura no arquivo são diferentes']
 
             MATRIZ_DE_DADOS.append(lista_de_dados)
         
@@ -65,22 +66,20 @@ class PDFReader():
 
     @classmethod
     def set_list(cls, directory: str) -> list:
-        lista_de_arquivos: list = [f'{directory}\\{arquivo}' for arquivo in listdir(directory) if arquivo.endswith('.pdf')]
+        lista_de_arquivos: list = [f'{directory}\\{arquivo}'
+                                    for arquivo in listdir(directory) 
+                                    if arquivo.endswith('.pdf') or arquivo.endswith('.xls') or arquivo.endswith('.xlsx')]
         return lista_de_arquivos
     
     @classmethod
     def get_carta_remessa(cls, lista_de_arquivos: list) -> str:
         for arquivo in lista_de_arquivos:
             
-            result: dict = cls.ler_pdfs(arquivo)
-            
-            if '(Carta)' in result['pages_content_array'][0]:
+            if '.xls' in arquivo.lower() or '.xlsx' in arquivo.lower():
                 return arquivo
-            
-        return False
 
     @classmethod                    
-    def ler_pdfs(cls, arquivo: str) -> dict[list, int]:
+    def ler_pdfs(cls, arquivo: str) -> dict:
         with open(arquivo, 'rb') as arquivo_pdf:
             leitor_pdf: PyPDF2.PdfReader = PyPDF2.PdfReader(arquivo_pdf)
             pages_array: list = []
@@ -98,49 +97,35 @@ class PDFReader():
         }
     
     @classmethod
-    def get_numero_nota(cls, array_pagina: list[str]) -> str:
+    def get_numero_nota(cls, array_pagina: list) -> str:
         for string in array_pagina:
             if 'Informações' in string:
                 return string.replace('Informações', '')
 
     @classmethod                               
     def get_df_remessa(cls, arquivo_carta_remessa: str) -> pd.DataFrame:
-        teste = read_pdf(arquivo_carta_remessa, pages='1')
-        df: pd.DataFrame = pd.DataFrame(teste[len(teste) - 1])
-        if 'Nome completo' in df.columns[0]:
-            matriz: list = df.values.tolist()
-
-        else:
-            matriz: list = [df.columns.to_list()]
-            for lista in df.values.tolist():
-                matriz.append(lista)
-
-        df_carta_remessa:pd.DataFrame = pd.DataFrame(matriz)
-        match len(df_carta_remessa.columns):
-            case 5:
-                df_carta_remessa.columns = ['Nº Fatura', 'Data de Emissão', 'Quantidade', 'Valor', 'Protocolo']
-            case 6:
-                df_carta_remessa.columns = ['Nº Fatura', 'Data de Emissão', 'Quantidade', 'Valor', 'Protocolo', 'Unnamed']
-            case 7:
-                df_carta_remessa.columns = ['Nº Fatura', 'Data de Emissão', 'Quantidade', 'Valor', 'Protocolo', 'Unnamed1', 'Unnamed2']
+        df_carta_remessa: pd.DataFrame = pd.read_excel(arquivo_carta_remessa, header=23)
+        df_carta_remessa = df_carta_remessa.iloc[:-6]
         
         return df_carta_remessa
     
     @classmethod
     def number_in_df(cls, df: pd.DataFrame, string_processo: str, string_protocolo) -> str | bool:
         for index, linha in df.iterrows():
-            if f"{linha['Nº Fatura']}" == string_processo and f"{linha['Protocolo']}" == string_protocolo:
-                return (f"{linha['Nº Fatura']}", f"{linha['Protocolo']}")
+            n_fatura: str = f"{linha['Nº Fatura']}".replace('.0', '')
+            n_protocolo: str = f"{linha['Protocolo']}".replace('.0', '')
+            if  n_fatura == string_processo and n_protocolo == string_protocolo:
+                return (n_fatura, n_protocolo)
             
             elif f"{linha['Nº Fatura']}" == string_processo and f"{linha['Protocolo']}" != string_protocolo:
-                return (f"{linha['Nº Fatura']}", False)
+                return (n_fatura, False)
             
             elif f"{linha['Nº Fatura']}" != string_processo and f"{linha['Protocolo']}" == string_protocolo:
-                return (False, f"{linha['Protocolo']}")
+                return (False, n_protocolo)
         return False
     
 class TreeView():
-    def __init__(self, dados: list[list], master: Tk=None):
+    def __init__(self, dados: list, master: Tk=None):
         self.tree: ttk.Treeview = ttk.Treeview(master, selectmode='browse', column=('column1', 'column2', 'column3', 'column4', 'column5', 'column6', 'column7'), show='headings')
 
         self.tree.column('column1', width=200, minwidth=50, stretch=YES)
@@ -172,10 +157,11 @@ class TreeView():
 def pdf_reader() -> None:
     caminho_do_pdf: str = askdirectory()
     pdf_reader: PDFReader = PDFReader(caminho_do_pdf)
-    dados = pdf_reader.main()
-    janela = Tk()
-    janela.iconbitmap('Robo.ico')
-    janela.title('Leitor de PDF GAMA')
-    janela.eval('tk::PlaceWindow . center')
-    tree_view = TreeView(dados, janela)
-    janela.mainloop()
+    dados: list | bool = pdf_reader.main()
+    if dados:
+        janela = Tk()
+        janela.iconbitmap('Robo.ico')
+        janela.title('Leitor de PDF GAMA')
+        janela.eval('tk::PlaceWindow . center')
+        tree_view = TreeView(dados, janela)
+        janela.mainloop()
